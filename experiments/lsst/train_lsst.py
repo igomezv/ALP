920c3fde3dfdb772ab9eb927bc46f7e63f790125
igomezv <neorelativista@gmail.com>
2026-03-11 17:22:05 +0100

    setting lsst sn1a experiment as example

--- experiments/lsst/train_lsst.py
+++ experiments/lsst/train_lsst.py
@@ -26,30 +26,39 @@ def main():
         z_train, y_train, np.concatenate([z_train, z_test]), np.concatenate([y_train, y_test])
     )
     
+    # Optimized hyperparameters from Optuna NSGA-II search
+    LEARNING_RATE = 0.00582938454299474
+    BATCH_SIZE = 16
+    DROPOUT = 0.1
+    DEEP_LAYERS = [150, 150]
+    EPOCHS = 1000
+    PATIENCE = 500
+    MC_DROPOUT_SAMPLES = 100
+    
     # Create and compile model
     logger.info("Creating ALP dual-output MLP...")
-    model = MLP(n_inputs=1, deep=[200, 200, 200, 200], dropout=0.1, mcdropout=True, n_outputs=2)
+    model = MLP(n_inputs=1, deep=DEEP_LAYERS, dropout=DROPOUT, mcdropout=True, n_outputs=2)
     keras_model = model.model_tf()
-    keras_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss="mse")
+    keras_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE), loss="mse")
     
     # Train model
     logger.info("Training model...")
     early_stopping = tf.keras.callbacks.EarlyStopping(
-        monitor="val_loss", patience=500, restore_best_weights=True, verbose=1
+        monitor="val_loss", patience=PATIENCE, restore_best_weights=True, verbose=1
     )
     history = keras_model.fit(
         z_train,
         y_train,
         validation_data=(z_test, y_test),
-        epochs=1000,
-        batch_size=16,
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
         verbose=2,
         callbacks=[early_stopping],
     )
     
     # Uncertainty quantification
     logger.info("Performing MC Dropout uncertainty quantification...")
-    uq = UncertaintyQuantifier(n_samples=100)
+    uq = UncertaintyQuantifier(n_samples=MC_DROPOUT_SAMPLES)
     z_test_range = np.linspace(np.min(z_data), np.max(z_data), 500)
     results = uq.mc_dropout_prediction(keras_model, z_test_range, scaler)
     
